fd19b1c5aa6934adf84e40ddaaa6a35ee04190b4
Suraj Pulloor <thegamer600@gmail.com>
2020-07-24 01:27:57 +0530

    Added the arth practise app

---
+++ practise/precache-manifest.328afa09f9a7b9b33271136c755f35c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803d3aa28aff7d125d5635e7c00d4676",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48162c479de25c62818f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "48162c479de25c62818f",
+    "url": "/static/js/2.3f36de1a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3f36de1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df111e28c8c9c55ca8a",
+    "url": "/static/js/main.ce0a24d4.chunk.js"
+  },
+  {
+    "revision": "f6396d70566116f97c1c",
+    "url": "/static/js/runtime-main.239c8a95.js"
+  }
+]);
